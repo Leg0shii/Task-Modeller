6bb285acd76fbe68853a40f63f64d8863c5b6ce3
0hnezahn <benjamin.mueller2@uni-rostock.de>
2022-08-06 01:40:07 +0200

    add CTTConnection.java - used for different edit window

---
+++ src/main/java/de/legoshi/taskmodeller/gui/symbol/connection/CTTConnection.java
@@ -0,0 +1,13 @@
+package de.legoshi.taskmodeller.gui.symbol.connection;
+
+import de.legoshi.taskmodeller.gui.symbol.ModelNode;
+import de.legoshi.taskmodeller.gui.windows.Workplace;
+
+public class CTTConnection extends Connection {
+
+    // different edit window
+    public CTTConnection(Workplace workplace, ModelNode node1, ModelNode node2) {
+        super(workplace, node1, node2);
+    }
+
+}
